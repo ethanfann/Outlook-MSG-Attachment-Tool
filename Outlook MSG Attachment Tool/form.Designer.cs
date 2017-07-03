b495071962cdfc24e236a190bf67db46756de7a9
Ethan Fann <ecfann@gmail.com>
2017-07-02 20:44:23 -0700

    Initial commit

---
+++ Outlook MSG Attachment Tool/form.Designer.cs
@@ -0,0 +1,221 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Outlook_MSG_Attachment_Tool
+{
+    sealed partial class DragForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DragForm));
+            this.detailLabel = new System.Windows.Forms.Label();
+            this.dragLabel = new System.Windows.Forms.Label();
+            this.settingsButton = new System.Windows.Forms.Button();
+            this.settingsPanel = new System.Windows.Forms.Panel();
+            this.pdfBox = new System.Windows.Forms.CheckBox();
+            this.deepSearchBox = new System.Windows.Forms.CheckBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.locationText = new System.Windows.Forms.TextBox();
+            this.locationBox = new System.Windows.Forms.CheckBox();
+            this.settingsTooltip = new System.Windows.Forms.ToolTip(this.components);
+            this.moveMsgTooltip = new System.Windows.Forms.ToolTip(this.components);
+            this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.deepSearchTip = new System.Windows.Forms.ToolTip(this.components);
+            this.locationBoxTip = new System.Windows.Forms.ToolTip(this.components);
+            this.pdfTip = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.settingsPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // detailLabel
+            // 
+            this.detailLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.detailLabel.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.detailLabel.Location = new System.Drawing.Point(0, 208);
+            this.detailLabel.Name = "detailLabel";
+            this.detailLabel.Size = new System.Drawing.Size(284, 51);
+            this.detailLabel.TabIndex = 1;
+            this.detailLabel.Text = "placeholder";
+            this.detailLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.detailLabel.Visible = false;
+            // 
+            // dragLabel
+            // 
+            this.dragLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dragLabel.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dragLabel.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.dragLabel.Location = new System.Drawing.Point(0, 0);
+            this.dragLabel.Name = "dragLabel";
+            this.dragLabel.Size = new System.Drawing.Size(284, 261);
+            this.dragLabel.TabIndex = 0;
+            this.dragLabel.Text = "Drag and Drop .msg file(s)";
+            this.dragLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // settingsButton
+            // 
+            this.settingsButton.BackColor = System.Drawing.Color.Transparent;
+            this.settingsButton.BackgroundImage = global::Outlook_MSG_Attachment_Tool.Properties.Resources.settings2;
+            this.settingsButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.settingsButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.settingsButton.FlatAppearance.BorderSize = 0;
+            this.settingsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.settingsButton.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.settingsButton.Location = new System.Drawing.Point(4, 0);
+            this.settingsButton.Name = "settingsButton";
+            this.settingsButton.Size = new System.Drawing.Size(28, 24);
+            this.settingsButton.TabIndex = 2;
+            this.settingsTooltip.SetToolTip(this.settingsButton, "Show/Hide settings");
+            this.settingsButton.UseVisualStyleBackColor = false;
+            this.settingsButton.Click += new System.EventHandler(this.settingsButton_Click);
+            // 
+            // settingsPanel
+            // 
+            this.settingsPanel.Controls.Add(this.pdfBox);
+            this.settingsPanel.Controls.Add(this.deepSearchBox);
+            this.settingsPanel.Controls.Add(this.browseButton);
+            this.settingsPanel.Controls.Add(this.locationText);
+            this.settingsPanel.Controls.Add(this.locationBox);
+            this.settingsPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.settingsPanel.Location = new System.Drawing.Point(0, 30);
+            this.settingsPanel.Name = "settingsPanel";
+            this.settingsPanel.Size = new System.Drawing.Size(284, 231);
+            this.settingsPanel.TabIndex = 6;
+            // 
+            // pdfBox
+            // 
+            this.pdfBox.AutoSize = true;
+            this.pdfBox.Checked = global::Outlook_MSG_Attachment_Tool.Properties.Settings.Default.createPDF;
+            this.pdfBox.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::Outlook_MSG_Attachment_Tool.Properties.Settings.Default, "createPDF", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.pdfBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.pdfBox.Location = new System.Drawing.Point(12, 79);
+            this.pdfBox.Name = "pdfBox";
+            this.pdfBox.Size = new System.Drawing.Size(485, 34);
+            this.pdfBox.TabIndex = 8;
+            this.pdfBox.Text = "Beta - Generate a PDF version of the .msg file";
+            this.pdfTip.SetToolTip(this.pdfBox, "Includes the To, From, CC, Send date, and Body fields.");
+            this.pdfBox.UseVisualStyleBackColor = true;
+            // 
+            // deepSearchBox
+            // 
+            this.deepSearchBox.AutoSize = true;
+            this.deepSearchBox.Checked = global::Outlook_MSG_Attachment_Tool.Properties.Settings.Default.deepSearch;
+            this.deepSearchBox.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::Outlook_MSG_Attachment_Tool.Properties.Settings.Default, "deepSearch", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.deepSearchBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.deepSearchBox.Location = new System.Drawing.Point(12, 55);
+            this.deepSearchBox.Name = "deepSearchBox";
+            this.deepSearchBox.Size = new System.Drawing.Size(200, 34);
+            this.deepSearchBox.TabIndex = 7;
+            this.deepSearchBox.Text = "Scan Subfolders";
+            this.deepSearchTip.SetToolTip(this.deepSearchBox, "Scan for additional .msg files stored in subfolders. Be default, only .msg files " +
+        "found in the original folder are processed.");
+            this.deepSearchBox.UseVisualStyleBackColor = true;
+            // 
+            // browseButton
+            // 
+            this.browseButton.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.browseButton.Enabled = false;
+            this.browseButton.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.browseButton.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.browseButton.Location = new System.Drawing.Point(209, 26);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(63, 22);
+            this.browseButton.TabIndex = 6;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = false;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // locationText
+            // 
+            this.locationText.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.locationText.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::Outlook_MSG_Attachment_Tool.Properties.Settings.Default, "savePath", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.locationText.Enabled = false;
+            this.locationText.Location = new System.Drawing.Point(23, 26);
+            this.locationText.Name = "locationText";
+            this.locationText.ReadOnly = true;
+            this.locationText.Size = new System.Drawing.Size(180, 37);
+            this.locationText.TabIndex = 5;
+            this.locationText.Text = global::Outlook_MSG_Attachment_Tool.Properties.Settings.Default.savePath;
+            // 
+            // locationBox
+            // 
+            this.locationBox.AutoSize = true;
+            this.locationBox.Checked = global::Outlook_MSG_Attachment_Tool.Properties.Settings.Default.singleLocation;
+            this.locationBox.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::Outlook_MSG_Attachment_Tool.Properties.Settings.Default, "singleLocation", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.locationBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.locationBox.Location = new System.Drawing.Point(12, 3);
+            this.locationBox.Name = "locationBox";
+            this.locationBox.Size = new System.Drawing.Size(278, 34);
+            this.locationBox.TabIndex = 4;
+            this.locationBox.Text = "Save to a single location";
+            this.locationBoxTip.SetToolTip(this.locationBox, "Output to a specified folder instead of the location of the original file(s).");
+            this.locationBox.UseVisualStyleBackColor = true;
+            this.locationBox.Click += new System.EventHandler(this.locationBox_Click);
+            // 
+            // DragForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.settingsPanel);
+            this.Controls.Add(this.settingsButton);
+            this.Controls.Add(this.detailLabel);
+            this.Controls.Add(this.dragLabel);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "DragForm";
+            this.Text = ".MSG Attachment Tool";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DragForm_FormClosing);
+            this.settingsPanel.ResumeLayout(false);
+            this.settingsPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Label detailLabel;
+        private Label dragLabel;
+        private Button settingsButton;
+        private Panel settingsPanel;
+        private ToolTip settingsTooltip;
+        private ToolTip moveMsgTooltip;
+        private Button browseButton;
+        private TextBox locationText;
+        private CheckBox locationBox;
+        private FolderBrowserDialog folderBrowser;
+        private CheckBox deepSearchBox;
+        private ToolTip deepSearchTip;
+        private ToolTip locationBoxTip;
+        private CheckBox pdfBox;
+        private ToolTip pdfTip;
+        private ToolTip toolTip1;
+    }
+}
+
---
+++ Outlook MSG Attachment Tool/form.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Outlook_MSG_Attachment_Tool.Properties;
+using Microsoft.Office.Interop.Outlook;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
+using PdfSharp.Pdf;
+using Color = System.Drawing.Color;
+
+
+namespace Outlook_MSG_Attachment_Tool
+{
+    public sealed partial class DragForm : Form
+    {
+        BackgroundWorker _worker = new BackgroundWorker();
+
+        public DragForm()
+        {
+            InitializeComponent();
+
+            AllowDrop = true;
+            DragEnter += form_DragEnter;
+            DragDrop += form_DragDrop;
+            DragOver += form_DragOver;
+            DragLeave += form_DragLeave;
+
+            _worker.DoWork += worker_DoWork;
+            _worker.RunWorkerCompleted += worker_Completed;
+
+            if(String.IsNullOrEmpty(Settings.Default.savePath))
+            {
+                Settings.Default.savePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Outlook MSG Tool";
+            }
+
+            settingsPanel.Hide();
+        }
+
+        void form_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        void form_DragOver(object sender, DragEventArgs e)
+        {
+            List<string> msgFilePaths = new List<string>((string[])e.Data.GetData(DataFormats.FileDrop)).FindAll(HasMsgExtension);
+            List<string> folderPaths = new List<string>((string[])e.Data.GetData(DataFormats.FileDrop)).FindAll(IsDirectory);
+
+            // Scan each submitted folder (and their subfolders, if enabled in settings) for .msg files.
+            foreach (string folder in folderPaths)
+            {
+                msgFilePaths.AddRange(Settings.Default.deepSearch
+                    ? Directory.GetFiles(folder, "*.msg*", SearchOption.AllDirectories)
+                    : Directory.GetFiles(folder, "*.msg*"));
+            }
+
+            if (msgFilePaths.Count >= 1)
+            {
+                BackColor = Color.MediumSeaGreen;
+
+            }
+            else
+            {
+                BackColor = Color.Red;
+            }
+            dragLabel.ForeColor = Color.White;
+        }
+
+        void form_DragLeave(object sender, EventArgs e)
+        {
+            BackColor = DefaultBackColor;
+            dragLabel.ForeColor = SystemColors.ButtonShadow;
+        }
+
+        void form_DragDrop(object sender, DragEventArgs e)
+        {
+            BackColor = DefaultBackColor;
+            dragLabel.ForeColor = SystemColors.ButtonShadow;
+
+            if (settingsPanel.Visible)
+            {
+                settingsPanel.Hide();
+                Settings.Default.Save();
+            }
+
+            Cursor.Current = Cursors.WaitCursor;
+
+            _worker.RunWorkerAsync(new List<string>((string[])e.Data.GetData(DataFormats.FileDrop)));
+        }
+
+        async Task delay()
+        {
+            await Task.Delay(5000);
+        }
+
+        async void ShowDetailLabel(string labelText)
+        {
+            detailLabel.Text = labelText;
+            detailLabel.Show();
+
+            await delay();
+
+            detailLabel.Hide();
+        }
+
+        private void settingsButton_Click(object sender, EventArgs e)
+        {
+            if (settingsPanel.Visible)
+            {
+                settingsPanel.Hide();
+            }
+            else
+            {
+                settingsPanel.Show();
+
+                if (Settings.Default.singleLocation)
+                {
+                    locationText.BackColor = SystemColors.Control;
+                    browseButton.BackColor = SystemColors.Control;
+
+                    locationText.Enabled = true;
+                    browseButton.Enabled = true;
+                }
+                else
+                {
+                    locationText.BackColor = SystemColors.ControlDark;
+                    browseButton.BackColor = SystemColors.ControlDark;
+
+                    locationText.Enabled = false;
+                    browseButton.Enabled = false;
+                }
+            }
+        }
+
+        /* 
+            Enables/Disables the single save location box and browse button based on the state of the 
+            single location save checkbox
+        */
+        private void locationBox_Click(object sender, EventArgs e)
+        {
+            Settings.Default.Save();
+
+            if (Settings.Default.singleLocation)
+            {
+                locationText.BackColor = SystemColors.Control;
+                browseButton.BackColor = SystemColors.Control;
+
+                locationText.Enabled = true;
+                browseButton.Enabled = true;
+            }
+            else
+            {
+                locationText.BackColor = SystemColors.ControlDark;
+                browseButton.BackColor = SystemColors.ControlDark;
+
+                locationText.Enabled = false;
+                browseButton.Enabled = false;
+            }
+        }
+
+        // Sets the value of the single location save path to the result of the folderbrowser dialog
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                Settings.Default.savePath = folderBrowser.SelectedPath;
+
+                Settings.Default.Save();
+            }
+        }
+
+        // Returns a pdf file containing useful information extracted from the msg file
+        private Document GeneratePdf(MailItem msg)
+        {
+            Document pdf = new Document();
+            Section section = pdf.AddSection();
+
+            section.AddParagraph("From: " + msg.SenderEmailAddress);
+            section.AddParagraph("To: " + msg.To);
+            
+            if (msg.CC != null)
+            {
+                section.AddParagraph("CC: " + msg.CC);
+            }
+
+            section.AddParagraph("Date Received: " + msg.ReceivedTime.ToString("M/d/yyyy h:mm tt"));
+
+            section.AddParagraph();
+
+            if (msg.Subject != null)
+            {
+                section.AddParagraph("Subject: " + msg.Subject);
+            }
+
+            section.AddParagraph();
+
+            if (msg.Body != null)
+            {
+                section.AddParagraph(msg.Body);
+            }
+
+            return pdf;
+        }
+
+        // Predicate for finding all files with the .msg extension in a list of paths
+        private static bool HasMsgExtension(string path)
+        {
+            return (Path.GetExtension(path) == ".msg");
+        }
+
+        // Predicate for finding all folders in a list of paths
+        private static bool IsDirectory(string path)
+        {
+            return ((File.GetAttributes(path) & FileAttributes.Directory) == FileAttributes.Directory);
+        }
+
+        private void DragForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Settings.Default.Save();
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            var ol = new Microsoft.Office.Interop.Outlook.Application();
+            List<string> msgFilePaths = ((List<string>) e.Argument).FindAll(HasMsgExtension);
+            List<string> folderPaths = ((List<string>) e.Argument).FindAll(IsDirectory);
+
+            // Scan each submitted folder (and their subfolders, if enabled in settings) for .msg files.
+            foreach (string folder in folderPaths)
+            {
+                msgFilePaths.AddRange(Settings.Default.deepSearch
+                    ? Directory.GetFiles(folder, "*.msg*", SearchOption.AllDirectories)
+                    : Directory.GetFiles(folder, "*.msg*"));
+            }
+
+            int attachmentCount = 0; // Stores total number of attachments. Displayed at end of processing in status text
+            foreach (string filePath in msgFilePaths)
+            {
+                // Use the outlook interop libary to create a MailItem object from the given .msg file
+                MailItem msg = ol.CreateItemFromTemplate(filePath);
+
+                string folderPath = "";
+                if(Settings.Default.singleLocation)
+                {
+                    // Test if the save path has been set if the single location save option is selected
+                    if(String.IsNullOrEmpty(Settings.Default.savePath))
+                    {
+                        e.Result = $"Please choose a save location or disable the single save location option in the settings menu";
+                        timer.Stop();
+                        return;
+                    }
+                    else
+                    {
+                        // Combine the single location save path and the file
+                        Console.WriteLine(Path.GetFileNameWithoutExtension(filePath));
+                        Console.WriteLine(filePath.Remove(filePath.Length - 4));
+                        folderPath = Settings.Default.singleLocation
+                            ? Path.Combine(Settings.Default.savePath, Path.GetFileNameWithoutExtension(filePath))
+                            : filePath.Remove(filePath.Length - 4);
+                    }
+   
+                }
+                else
+                {
+                    folderPath = filePath.Remove(filePath.Length - 4);
+                }
+
+                if (Directory.Exists(folderPath))
+                {
+                    // Adds the current time to the end of a foldername in the event of duplicate folders being created
+                    folderPath = folderPath + " " + DateTime.Now.ToString("MM-dd-yyyy HHmmss");
+                    Directory.CreateDirectory(folderPath);
+                }
+                else
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+
+                if (Settings.Default.createPDF)
+                {
+                    Document pdf = GeneratePdf(msg);
+
+                    PdfDocumentRenderer renderer = new PdfDocumentRenderer(false, PdfFontEmbedding.Always)
+                    {
+                        Document = pdf
+                    };
+
+                    renderer.RenderDocument();
+                    renderer.PdfDocument.Save(Path.Combine(folderPath, msg.Subject + ".pdf"));
+                }
+
+                foreach (Attachment attachment in msg.Attachments)
+                {
+                    attachment.SaveAsFile(Path.Combine(folderPath, attachment.FileName));
+                    attachmentCount++;
+                }
+            }
+            timer.Stop();
+
+            e.Result =
+                $"Processed {msgFilePaths.Count} .msg Files(s) and extracted {attachmentCount} attachment(s) in {timer.ElapsedMilliseconds/1000.0} seconds.";
+
+        }
+   
+        public void worker_Completed(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Cursor.Current = Cursors.Default;
+            ShowDetailLabel(e.Result.ToString());
+        }
+
+    }
+}
+
+
